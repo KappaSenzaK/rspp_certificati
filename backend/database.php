7db1e81136be6a09db0fc1dbc6da7bacea8f56fb
franchiStud <107928429+franchiStud@users.noreply.github.com>
2023-03-04 10:09:11 +0100

    Add files via upload

--- backend/database.php
+++ backend/database.php
@@ -1,30 +1,61 @@
 <?php
+    function connect_database($server_name = "localhost",
+                            $username = "root",
+                            $password = "",
+                            $database = "rspp_certificati")
+    {
+        $conn = new mysqli($server_name, $username, $password);
+        if($conn->connect_error){
+            die("Connessione fallita: " . $conn->connect_error);
+        }
 
-function connect_database($server_name = "localhost",
-                          $username = "root",
-                          $password = "",
-                          $database = "rspp_certificati")
-{
-    $conn = new mysqli($server_name, $username, $password);
-    if($conn->connect_error){
-        die("Connessione fallita: " . $conn->connect_error);
+        $retval = mysqli_select_db( $conn, $database );
+        if(! $retval ) {
+            die('Impossibile selezionare il database: ' . mysqli_error($conn));
+        }
+
+        return $conn;
     }
 
-    $retval = mysqli_select_db( $conn, $database );
-    if(! $retval ) {
-        die('Impossibile selezionare il database: ' . mysqli_error($conn));
-    }
+    function make_query($query){
+        $conn = connect_database();
 
-    return $conn;
-}
-
-function make_query($query){
-    $conn = connect_database();
-
-    if ($result = $conn->query($query)) {
+        if ($result = $conn->query($query)) {
+            $conn->close();
+            return $result;
+        }
         $conn->close();
-        return $result;
+        return null;
     }
-    $conn->close();
-    return null;
-}
+
+    function obtain_password($mail) {
+        $results = make_query("SELECT mail, pw FROM personale");
+
+        foreach($results as $row) {
+            if($row['mail'] == $mail)
+                return $row['pw'];
+        }
+        return null;
+    }
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Massimo Cuccurullo Official Fan Page
+
+    membri:<br>
+    <?php
+        $results = make_query("SELECT * FROM personale");
+
+        foreach($results as $row) {
+            echo $row['nome'] . " " . $row['cognome'] . "<br>";
+        }
+    ?>
+</body>
+</html>
