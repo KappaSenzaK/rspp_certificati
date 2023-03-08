374a72b08e7bc43b6bc8aa9cbed05f9dda747444
KappaSenzaK <stefano.hu1.stud@tulliobuzzi.edu.it>
2023-03-08 16:15:09 +0100

    small fix

--- auth/login.php
+++ auth/login.php
@@ -9,7 +9,7 @@ $request = json_decode(file_get_contents('php://input', true));
 $password_db = obtain_password($request->email);
 $hashed_password = hash('sha256', $request->password);
 
-// perche i dati predifiniti non sono crittografati (digest)
+// perche i dati predefiniti non sono crittografati (digest)
 if ($DEBUG) $password_db = hash('sha256', $password_db);
 
 if ($password_db != $hashed_password){
