9061d20cac1cd8431fa8c7969fcee7d2e2700bd9
franchiStud <107928429+franchiStud@users.noreply.github.com>
2023-03-18 10:22:47 +0100

    help()

--- js/user_form.js
+++ js/user_form.js
@@ -1,4 +1,13 @@
-let user_name = "a";
-let user_surname = "b";
+var helpStatus = false
 
-document.getElementById("welcome").innerHTML = user_name + " " + user_surname;
+function help() {
+    if(helpStatus) {
+        helpStatus = false;
+        document.getElementById("help").innerHTML = "";
+    } else {
+        helpStatus = true;
+        document.getElementById("help").innerHTML = "In questa schermata puoi inserire i certificati sulla sicurezza di cui sei gia in possesso.<br>" +
+                                                    "Premi sul pulsante 'Scegli il file' per selezionare il file .pdf del certificato che vuoi inviare dal tuo dispositivo.<br>" + 
+                                                    "Attenzione: se possiedi un certificato specifico, oltre al file .pdf dovrai inserire anche la data di scadenza !";
+    }
+}
