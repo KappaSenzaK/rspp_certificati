ea9052150f6dcc5285941beb29a2d185d0f508f5
studente studente <s.buzzi@Buzzi.Aule>
2023-03-18 09:43:37 +0100

    added PHPMailer lib

---
+++ email/sendmail.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$to_email = "stefano.hu1.stud@tulliobuzzi.edu.it";
+$subject = "Test email to send from XAMPP";
+$body = "Hi, This is test mail to check how to send mail from Localhost Using Gmail ";
+$headers = "From: sender email";
+
+if (mail($to_email, $subject, $body, $headers))
+{
+    echo "Email successfully sent to $to_email...";
+}
+else
+{
+    echo "Email sending failed!";
+}
