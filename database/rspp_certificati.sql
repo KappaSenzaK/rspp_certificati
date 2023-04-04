1e6996a8304e910b16ff8421680c9e49754321b8
franchiStud <107928429+franchiStud@users.noreply.github.com>
2023-04-04 08:43:46 +0200

    aggiornamento view

--- database/rspp_certificati.sql
+++ database/rspp_certificati.sql
@@ -11,7 +11,7 @@ CREATE TABLE personale (
 	-- nel campo mail sarà inserita solo la prima parte dell'indirizzo
 	-- (es. "vittorio.zhang.stud@tulliobuzzi.edu.it" -> solo "vittorio.zhang.stud")
 	-- si da quindi per scontato che il dominio sia "@tulliobuzzi.edu.it"
-	mail         VARCHAR(64) PRIMARY KEY NOT NULL, --CHECK(mail NOT LIKE '%.stud')
+	mail         VARCHAR(64) PRIMARY KEY NOT NULL, -- CHECK(mail NOT LIKE '%.stud')
 	tipo         ENUM('ata','docente')   NOT NULL,
 	nome         VARCHAR(32)             NOT NULL,
 	cognome      VARCHAR(32)             NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE personale (
 CREATE TABLE attestato (
 	mail VARCHAR(64) NOT NULL,
 	tipo ENUM('formazione_generale',
-		  'formazione_specifica_medio'
+		  'formazione_specifica_medio',
 		  'formazione_alto',
 		  'formazione_sicurezza_preposto',
 		  'aggiornamento_sicurezza_preposto',
@@ -36,7 +36,7 @@ CREATE TABLE attestato (
 		  'aggiornamento_rspp',
 		  'formazione_incendio_medio',
 		  'formazione_incendio_alto',
-		  'formazione_primo_soccorso'
+		  'formazione_primo_soccorso',
 		  'aggiornamento_primo_soccorso',
 		  'formazione_blsd',
 		  'aggiornamento_blsd',
@@ -75,28 +75,52 @@ CREATE VIEW personale_validato AS
 	FROM personale p
 	WHERE p.stato = 'Validato';
 
-CREATE VIEW personale_richiesta modifica AS
+CREATE VIEW personale_richiesta_modifica AS
 	SELECT p.*
 	FROM personale p
 	WHERE p.stato = 'Richiesta modifica';
 
--- SCADUTO
--- a_s.data_scadenza < CURRENT_DATE;
 
--- IN SCADENZA			
--- (
--- ((MONTH(a_s.data_scadenza) >= (MONTH(CURRENT_DATE)-2) AND MONTH(a_s.data_scadenza) > 2) AND YEAR(a_s.data_scadenza) = YEAR(CURRENT_DATE)) OR
--- (((MONTH(CURRENT_DATE) = 12) AND MONTH(a_s.data_scadenza) = 2) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza)) OR
--- (((MONTH(CURRENT_DATE) = 11) AND MONTH(a_s.data_scadenza) = 1) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza))
--- )
--- AND a_s.data_scadenza >= CURRENT_DATE;
-
-
+CREATE VIEW personale_in_scadenza AS
+	SELECT p.*
+	FROM personale p
+	WHERE p.mail IN (
+		SELECT p1.mail
+		FROM personale p1
+		WHERE (
+			SELECT COUNT(1)
+			FROM personale p2
+				INNER JOIN attestato a_s ON a_s.data_scadenza IS NOT NULL AND a_s.mail = p2.mail
+			WHERE p2.mail = p1.mail
+				AND
+				(
+				 ((MONTH(a_s.data_scadenza) >= (MONTH(CURRENT_DATE)-2) AND MONTH(a_s.data_scadenza) > 2) AND YEAR(a_s.data_scadenza) = YEAR(CURRENT_DATE)) OR
+				 (((MONTH(CURRENT_DATE) = 12) AND MONTH(a_s.data_scadenza) = 2) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza)) OR
+				 (((MONTH(CURRENT_DATE) = 11) AND MONTH(a_s.data_scadenza) = 1) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza))
+				)
+				AND a_s.data_scadenza >= CURRENT_DATE
+		) > 0
+	)
+	
+CREATE VIEW personale_in_scadenza AS
+	SELECT p.*
+	FROM personale p
+	WHERE p.mail IN (
+		SELECT p1.mail
+		FROM personale p1
+		WHERE (
+			SELECT COUNT(1)
+			FROM personale p2
+				INNER JOIN attestato a_s ON a_s.data_scadenza IS NOT NULL AND a_s.mail = p2.mail
+			WHERE p2.mail = p1.mail
+				AND a_s.data_scadenza < CURRENT_DATE;
+		) > 0
+	)
+	
 -- INSERIMENTO DI DATI a caso
-
-INSERT INTO personale (mail, tipo, nome, cognome, cod_fiscale, pw)
-VALUES  ('ettore.franchi', 'docente', 'Ettore', 'Franchi', 'FRNTTR04R27D612A', '1234'),
-	('zhario.zhang', 'ata', 'Zhario', 'Zhang', 'ZHRZNG420CHINA11', '1234'),
-	('stefano.hu', 'docente', 'Stefano', 'Hu', 'SFNHU12UU74', '1234'),
-	('marco.carricato', 'ata', 'Marco', 'Carricato', 'MRCCRR696969', '1234'),
-	('dio.brando', 'docente', 'Dio', 'Brando', 'DIOBRN6629012F', '1234');
+INSERT INTO personale (mail, tipo, nome, cognome, cod_fiscale)
+VALUES  ('ettore.franchi', 'docente', 'Ettore', 'Franchi', 'FRNTTR04R27D612A'),
+	('zhario.zhang', 'ata', 'Zhario', 'Zhang', 'ZHRZNG420CHINA11'),
+	('stefano.hu', 'docente', 'Stefano', 'Hu', 'SFNHU12UU74'),
+	('marco.carricato', 'ata', 'Marco', 'Carricato', 'MRCCRR696969'),
+	('dio.brando', 'docente', 'Dio', 'Brando', 'DIOBRN6629012F');
