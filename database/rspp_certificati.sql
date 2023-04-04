182e7daf892f2ae0fffa944036d0865e09d2ac9b
franchiStud <107928429+franchiStud@users.noreply.github.com>
2023-04-04 08:13:54 +0200

    aggiornato ddl, da finire dml

--- database/rspp_certificati.sql
+++ database/rspp_certificati.sql
@@ -12,13 +12,12 @@ CREATE TABLE personale (
 	-- nel campo mail sarà inserita solo la prima parte dell'indirizzo
 	-- (es. "vittorio.zhang.stud@tulliobuzzi.edu.it" -> solo "vittorio.zhang.stud")
 	-- si da quindi per scontato che il dominio sia "@tulliobuzzi.edu.it"
-	mail         VARCHAR(64) PRIMARY KEY NOT NULL,
+	mail         VARCHAR(64) PRIMARY KEY NOT NULL, --CHECK(mail NOT LIKE '%.stud')
 	tipo         ENUM('ata','docente')   NOT NULL,
 	nome         VARCHAR(32)             NOT NULL,
 	cognome      VARCHAR(32)             NOT NULL,
 	-- il codice fiscale è diverso per ogni paese, perciò ho messo VARCHAR
 	cod_fiscale  VARCHAR(32)             NOT NULL,
-	data_nascita DATE                    NOT NULL,
 	note         VARCHAR(128)            NOT NULL,
         stato 	     ENUM('Da compilare', 'Da validare', 'Validato', 'Richiesta modifica') DEFAULT 'Da compilare',
 	pw           VARCHAR(32)             NOT NULL DEFAULT '1234'
@@ -43,7 +42,8 @@ CREATE TABLE attestato (
 		  'formazione_blsd',
 		  'aggiornamento_blsd',
 		  'altro') NOT NULL,
-	descrizione VARCHAR(128),
+	descrizione   VARCHAR(128),
+	data_scadenza DATE,
 	PRIMARY KEY(mail,tipo),
 	CONSTRAINT attestato_generico_mail
 		FOREIGN KEY (mail)
@@ -51,76 +51,53 @@ CREATE TABLE attestato (
 );
 
 -- VIEW
-CREATE VIEW personale_generale AS
-	SELECT p.*, a_g.mail AS attestato_generico, a_s.mail AS attestato_specifico, a_s.data_scadenza
-	FROM personale p
-		LEFT JOIN attestato_generico  a_g ON a_g.mail = p.mail
-		LEFT JOIN attestato_specifico a_s ON a_s.mail = p.mail;
-
 CREATE VIEW personale_docente AS
-	SELECT p.*, a_g.mail AS attestato_generico, a_s.mail AS attestato_specifico, a_s.data_scadenza
+	SELECT p.*
 	FROM personale p
-		LEFT JOIN attestato_generico  a_g ON a_g.mail = p.mail
-		LEFT JOIN attestato_specifico a_s ON a_s.mail = p.mail
 	WHERE p.tipo = 'docente';
 
 CREATE VIEW personale_ata AS
-	SELECT p.*, a_g.mail AS attestato_generico, a_s.mail AS attestato_specifico, a_s.data_scadenza
+	SELECT p.*
 	FROM personale p
-		LEFT JOIN attestato_generico  a_g ON a_g.mail = p.mail
-		LEFT JOIN attestato_specifico a_s ON a_s.mail = p.mail
 	WHERE p.tipo = 'ata';
 
-CREATE VIEW personale_generale_da_compilare AS
-	SELECT p.*, a_g.mail AS attestato_generico, a_s.mail AS attestato_specifico, a_s.data_scadenza
+CREATE VIEW personale_da_compilare AS
+	SELECT p.*
 	FROM personale p
-		LEFT JOIN attestato_generico  a_g ON a_g.mail = p.mail
-		LEFT JOIN attestato_specifico a_s ON a_s.mail = p.mail
 	WHERE p.stato = 'Da compilare';
 
-CREATE VIEW personale_generale_da_revisionare AS
-	SELECT p.*, a_g.mail AS attestato_generico, a_s.mail AS attestato_specifico, a_s.data_scadenza
+CREATE VIEW personale_da_validare AS
+	SELECT p.*
 	FROM personale p
-		LEFT JOIN attestato_generico  a_g ON a_g.mail = p.mail
-		LEFT JOIN attestato_specifico a_s ON a_s.mail = p.mail
-	WHERE p.stato = 'Da revisionare';
+	WHERE p.stato = 'Da validare';
+	
+CREATE VIEW personale_validato AS
+	SELECT p.*
+	FROM personale p
+	WHERE p.stato = 'Validato';
 
-CREATE VIEW personale_generale_attestato_specifico_scaduto AS
-	SELECT p.*, a_g.mail AS attestato_generico, a_s.mail AS attestato_specifico, a_s.data_scadenza
+CREATE VIEW personale_richiesta modifica AS
+	SELECT p.*
 	FROM personale p
-		LEFT  JOIN attestato_generico  a_g ON a_g.mail = p.mail
-		INNER JOIN attestato_specifico a_s ON a_s.mail = p.mail
-	WHERE a_s.data_scadenza < CURRENT_DATE;
+	WHERE p.stato = 'Richiesta modifica';
 
-CREATE VIEW personale_generale_attestato_specifico_in_scadenza AS
-	SELECT p.*, a_g.mail AS attestato_generico, a_s.mail AS attestato_specifico, a_s.data_scadenza
-	FROM personale p
-		LEFT  JOIN attestato_generico  a_g ON a_g.mail = p.mail
-		INNER JOIN attestato_specifico a_s ON a_s.mail = p.mail
-	WHERE (
-			((MONTH(a_s.data_scadenza) >= (MONTH(CURRENT_DATE)-2) AND MONTH(a_s.data_scadenza) > 2) AND YEAR(a_s.data_scadenza) = YEAR(CURRENT_DATE)) OR
-		  	(((MONTH(CURRENT_DATE) = 12) AND MONTH(a_s.data_scadenza) = 2) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza)) OR
-		  	(((MONTH(CURRENT_DATE) = 11) AND MONTH(a_s.data_scadenza) = 1) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza))
-		)
-			AND a_s.data_scadenza >= CURRENT_DATE;
+-- SCADUTO
+-- a_s.data_scadenza < CURRENT_DATE;
+
+-- IN SCADENZA			
+-- (
+-- ((MONTH(a_s.data_scadenza) >= (MONTH(CURRENT_DATE)-2) AND MONTH(a_s.data_scadenza) > 2) AND YEAR(a_s.data_scadenza) = YEAR(CURRENT_DATE)) OR
+-- (((MONTH(CURRENT_DATE) = 12) AND MONTH(a_s.data_scadenza) = 2) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza)) OR
+-- (((MONTH(CURRENT_DATE) = 11) AND MONTH(a_s.data_scadenza) = 1) AND YEAR(CURRENT_DATE) + 1 = YEAR(a_s.data_scadenza))
+-- )
+-- AND a_s.data_scadenza >= CURRENT_DATE;
+
 
 -- INSERIMENTO DI DATI a caso
 
-INSERT INTO personale (mail, tipo, nome, cognome, cod_fiscale, data_nascita)
-VALUES ('ettore.franchi', 'docente', 'Ettore', 'Franchi', 'FRNTTR04R27D612A', '1980-01-01');
-INSERT INTO personale (mail, tipo, nome, cognome, cod_fiscale, data_nascita)
-VALUES ('marco.carricato', 'docente', 'Marco', 'Carricato', 'DIO242SQ1', '1923-01-01');
-INSERT INTO personale (mail, tipo, nome, cognome, cod_fiscale, data_nascita)
-VALUES ('jonathan.joestar', 'ata', 'Jonathan', 'Joestar', 'DIO242SQ1SDF2', '1980-04-05');
-INSERT INTO personale (mail, tipo, nome, cognome, cod_fiscale, data_nascita)
-VALUES ('dio.brando', 'ata', 'Dio', 'Brando', 'SDF2SDFG23SD', '1803-04-01');
-INSERT INTO `personale` (`mail`, `tipo`, `nome`, `cognome`, `cod_fiscale`, `data_nascita`, `stato`)
-VALUES ('zhario.zhang', 'ata', 'Zhario', 'Zhang', 'ZHRZHN04CHINA420', '2003-07-09', 'Da compilare');
-
-INSERT INTO attestato_generico(mail) VALUES ('ettore.franchi');
-INSERT INTO attestato_generico(mail) VALUES ('dio.brando');
-INSERT INTO attestato_generico(mail) VALUES ('zhario.zhang');
-
-INSERT INTO attestato_specifico (mail, data_scadenza) VALUES ('marco.carricato',  '2004-04-04');
-INSERT INTO attestato_specifico (mail, data_scadenza) VALUES ('jonathan.joestar', '2009-01-30');
-INSERT INTO attestato_specifico (mail, data_scadenza) VALUES ('zhario.zhang', '2023-04-30');
+INSERT INTO personale (mail, tipo, nome, cognome, cod_fiscale, pw)
+VALUES  ('ettore.franchi', 'docente', 'Ettore', 'Franchi', 'FRNTTR04R27D612A', '1234'),
+	('zhario.zhang', 'ata', 'Zhario', 'Zhang', 'ZHRZNG420CHINA11', '1234'),
+	('stefano.hu', 'docente', 'Stefano', 'Hu', 'SFNHU12UU74', '1234'),
+	('marco.carricato', 'ata', 'Marco', 'Carricato', 'MRCCRR696969', '1234'),
+	('dio.brando', 'docente', 'Dio', 'Brando', 'DIOBRN6629012F', '1234');
