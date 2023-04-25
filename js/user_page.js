6b7b7f2354bd5fd8e32b0fd5af9748f981fb8437
franchiStud <107928429+franchiStud@users.noreply.github.com>
2023-04-25 08:31:26 +0200

    aggiornata userpage

--- js/user_page.js
+++ js/user_page.js
@@ -10,6 +10,11 @@ function help() {
                                                     "Premi sul pulsante 'Visualizza pdf' per visualizzare il documento in questione.<br>" +
                                                     "Se sei in possesso di un certificato specifico, potrai anche leggere la data di scadenza.<br>" +
                                                     "Se necessiti di fare una modifica ai dati, puoi premere sul pulsante 'Richiedi modifica',<br>" +
-                                                    "così che l'amministratore possa darti la possibilità di ri-inserire i dati.";
+                                                    "così che l'amministratore possa darti la possibilità di ri-inserire i dati.<br>" +
+                                                    "Attenzione! Se non riesci a vedere il pulsante 'Richiedi modifica', significa che l'amministratore<br>" +
+                                                    "non ha ancora validato i tuoi dati, solo una volta che tutti gli attestati che hai inserito" +
+                                                    "saranno controllati potrai modificarli di nuovo.<br>" +
+                                                    "Se l'amministratore sta impiegando molto tempo a validare i tuoi attestati, ti consigliamo di" +
+                                                    "contattarlo via e-mail o attraverso la segreteria scolastica.";
     }
 }
