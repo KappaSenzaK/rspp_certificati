44a64d86573b31793ac398ebee285801be25a5cf
franchiStud <107928429+franchiStud@users.noreply.github.com>
2023-04-28 15:07:19 +0200

    inserimento cert user

---
+++ js/user_form_select.js
@@ -0,0 +1,13 @@
+let helpStatus = false;
+
+function help() {
+    if(helpStatus) {
+        helpStatus = false;
+        document.getElementById("help").innerHTML = "";
+    } else {
+        helpStatus = true;
+        document.getElementById("help").innerHTML = 
+            "Seleziona il tipo di certificato che vuoi inviare.<br>"+
+            "Se hai delle certificazioni ma non sai a che tipo associarle, clicca su 'Altro'.<br><br><br>";
+    }
+}
