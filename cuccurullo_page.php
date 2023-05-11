fb6329699b97556941903d6072c593193a4de798
Cangiolone <93873816+MeldrisSama@users.noreply.github.com>
2023-05-11 19:41:27 +0200

    Non c'è bisogno di dire niente
    Il grande scherzo

--- cuccurullo_page.php
+++ cuccurullo_page.php
@@ -2,7 +2,7 @@
 <?php
     session_start();
     include 'checkPage.php';
-    if($_SESSION['mail'] == 'rspp') {
+    if($_SESSION['mail'] != 'rspp') {
         ?>
             <h1>Errore nell'accesso!</h1><br><br><br>
             <button id="helpButton" class="button" onclick="window.open('index.php'); window.close()"> Torna alla pagina di accesso </button>
