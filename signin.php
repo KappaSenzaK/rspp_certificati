9b8f6c85f4dea9d27bc0ff944a40dfed275bc6f4
franchiStud <107928429+franchiStud@users.noreply.github.com>
2023-06-01 16:51:40 +0200

    hash password, grafica pulsante

--- signin.php
+++ signin.php
@@ -3,12 +3,12 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Creazione dell'account</title>
-  <link rel="stylesheet" href="css/button.css">
-  <link rel="stylesheet" href="css/font.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creazione dell'account</title>
+    <link rel="stylesheet" href="css/button.css">
+    <link rel="stylesheet" href="css/font.css">
 </head>
 <?php
 session_start();
@@ -20,35 +20,34 @@ include 'html/header.html';
 ?>
 <script src="js/login.js"></script>
 <head>
-  <title>Creazione dell'account</title>
+    <title>Creazione dell'account</title>
 </head>
 <div class="font" align="center"><h1>Creazione di un nuovo account</h1></div>
 <br>
 <?php
 
-if ( ! isset($_SESSION['signin'])) {
+if (!isset($_SESSION['signin'])) {
     $_SESSION['signin'] = true;
 }
 
 if (isset($_POST['back'])) {
-    if (isset($_SESSION['code'])) {
+    if (isset($_SESSION['code']))
         unset($_SESSION['code']);
-    }
 }
 
 if (isset($_SESSION['code'])) {
-    if (isset($_POST['code'])) {
+    if (isset($_POST['code']))
         if ($_SESSION['code'] == $_POST['code']) {
             // createNewAccount($mail, $tipo, $nome, $cognome, $cod_fiscale, $pw)
             createNewAccount(
-                    $_SESSION['signin_mail'],
-                    $_SESSION['signin_tipo'],
-                    $_SESSION['signin_nomeUtente'],
-                    $_SESSION['signin_cognomeUtente'],
-                    $_SESSION['signin_codiceFiscale'],
-                    $_SESSION['signin_dataNascita'],
-                    $_SESSION['signin_luogoNascita'],
-                    $_SESSION['code']
+                $_SESSION['signin_mail'],
+                $_SESSION['signin_tipo'],
+                $_SESSION['signin_nomeUtente'],
+                $_SESSION['signin_cognomeUtente'],
+                $_SESSION['signin_codiceFiscale'],
+                $_SESSION['signin_dataNascita'],
+                $_SESSION['signin_luogoNascita'],
+                $_SESSION['code']
             );
 
             echo '<div class="font" align="center">
@@ -57,7 +56,7 @@ if (isset($_SESSION['code'])) {
                     Ricorda che i dati che hai inserito passeranno sotto la revisione dell\'RSPP scolastico,
                     perciò potrebbero variare nel caso in cui ci siano delle incongruenze!<br><br><br>
                     <form action="index.php" method="post">
-                        <input type="submit" value="Torna alla pagina di accesso">
+                        <input class="button" type="submit" value="Torna alla pagina di accesso">
                     </form>
                  </div>';
 
@@ -76,7 +75,6 @@ if (isset($_SESSION['code'])) {
                     </form>
                  </div>';
         }
-    }
     die();
 }
 
@@ -88,13 +86,13 @@ if ($_SESSION['signin']) {
     die();
 }
 
-if ( ! isset($_POST['email'])
-        || ! isset($_POST['tipo'])
-        || ! isset($_POST['nomeUtente'])
-        || ! isset($_POST['cognomeUtente'])
-        || ! isset($_POST['codiceFiscale'])
-        || ! isset($_POST['dataNascita'])
-        || ! isset($_POST['luogoNascita'])
+if (!isset($_POST['email'])
+    || !isset($_POST['tipo'])
+    || !isset($_POST['nomeUtente'])
+    || !isset($_POST['cognomeUtente'])
+    || !isset($_POST['codiceFiscale'])
+    || !isset($_POST['dataNascita'])
+    || !isset($_POST['luogoNascita'])
 ) {
     include 'html/signin.html';
 
@@ -102,89 +100,87 @@ if ( ! isset($_POST['email'])
     die();
 }
 
-$email         = $_POST['email'];
-$tipo          = $_POST['tipo'];
-$nomeUtente    = $_POST['nomeUtente'];
+$email = $_POST['email'];
+$tipo = $_POST['tipo'];
+$nomeUtente = $_POST['nomeUtente'];
 $cognomeUtente = $_POST['cognomeUtente'];
 $codiceFiscale = $_POST['codiceFiscale'];
-$dataNascita   = $_POST['dataNascita'];
-$luogoNascita  = $_POST['luogoNascita'];
+$dataNascita = $_POST['dataNascita'];
+$luogoNascita = $_POST['luogoNascita'];
 
-$_SESSION['signin_mail']          = $email;
-$_SESSION['signin_tipo']          = $tipo;
-$_SESSION['signin_nomeUtente']    = $nomeUtente;
+$_SESSION['signin_mail'] = $email;
+$_SESSION['signin_tipo'] = $tipo;
+$_SESSION['signin_nomeUtente'] = $nomeUtente;
 $_SESSION['signin_cognomeUtente'] = $cognomeUtente;
 $_SESSION['signin_codiceFiscale'] = $codiceFiscale;
-$_SESSION['signin_dataNascita']   = $dataNascita;
-$_SESSION['signin_luogoNascita']  = $luogoNascita;
+$_SESSION['signin_dataNascita'] = $dataNascita;
+$_SESSION['signin_luogoNascita'] = $luogoNascita;
 
 
 function firstLetterToUpperCase($string)
 {
     if (ctype_upper($string[0])) {
-        return strtoupper(substr($string, 0, 1)).substr($string, 1, strlen($string) - 1);
+        return strtoupper(substr($string, 0, 1)) . substr($string, 1, strlen($string) - 1);
     }
-
     return $string;
 }
 
-$nomeUtente    = firstLetterToUpperCase($nomeUtente);
+$nomeUtente = firstLetterToUpperCase($nomeUtente);
 $cognomeUtente = firstLetterToUpperCase($cognomeUtente);
 $codiceFiscale = strtoupper($codiceFiscale);
-$luogoNascita  = firstLetterToUpperCase($luogoNascita);
+$luogoNascita = firstLetterToUpperCase($luogoNascita);
 if (existAccountByEmail($email)) {
     $_SESSION['signin'] = true;
     die("<br><h1>Attenzione: l'account è già esistente!</h1><br><h1><button class='button' onclick='openLogin()'>Effettua il login! </button></h1>");
 }
 
 $digestPassword = substr(hash(
-        'md5',
-        $email.$tipo.$nomeUtente.$cognomeUtente.$codiceFiscale.$dataNascita.$luogoNascita),
-        0, 5
+    'md5',
+    $email . $tipo . $nomeUtente . $cognomeUtente . $codiceFiscale . $dataNascita . $luogoNascita),
+    0, 5
 );
 
 $sendmail_message = sendEmail(
-        $email."@tulliobuzzi.edu.it",
-        "Credenziali RSPP Certificati",
-        "<h1>Password per l'accesso all'account: ".$digestPassword."</h1>"
+    $email . "@tulliobuzzi.edu.it",
+    "Credenziali RSPP Certificati",
+    "<h1>Password per l'accesso all'account: " . $digestPassword . "</h1>"
 );
 
 if ($sendmail_message != EmailStatus::OK) {
-    echo "<h2>".$sendmail_message."</h2>";
+    echo "<h2>" . $sendmail_message . "</h2>";
     die();
 }
 
 $_SESSION['signin'] = true;
-$_SESSION['code']   = $digestPassword;
+$_SESSION['code'] = $digestPassword;
 
 ?>
 <div class="font">
-  <div align="center">
+    <div align="center">
 
-    <h2>All'indirizzo e-mail <?php
-        echo $email; ?>@tulliobuzzi.edu.it
-      è stata inviata la tua nuova password</h2> <br>
+        <h2>All'indirizzo e-mail <?php echo $email; ?>@tulliobuzzi.edu.it
+            è stata inviata la tua nuova password</h2> <br>
 
-    Controlla bene che i dati che hai inserito siano corretti:<br>
-    <h3>
-        <?php
-        echo "Indirizzo e-mail: ".$email."<br>".
-                "Tipologia di utente: ".$tipo."<br>".
-                "Nome e cognome: ".$nomeUtente." ".$cognomeUtente."<br>".
-                "Codice fiscale: ".$codiceFiscale."<br>".
-                "Data di nascita: ".$dataNascita."<br>".
-                "Luogo di nascita: ".$luogoNascita."<br>";
-        ?>
-    </h3><br>
-    <button class='button' onclick='location.reload()'> I tuoi dati non sono corretti? Torna all'inserimento
-    </button>
-    <br><br><br>
-    Se tutti i dati sono corretti, inserisci qui sotto il codice che ti è stato inviato via e-mail per creare il tuo
-    account<br>
-    <br>
-    <form class="button" action="signin.php" method="post">
-      <input type="text" name="code" id="code" placeholder="Codice">
-      <input type="submit">
-    </form>
-  </div>
+        Controlla bene che i dati che hai inserito siano corretti:<br>
+        <h3>
+            <?php
+            echo "Indirizzo e-mail: " . $email . "<br>" .
+                "Tipologia di utente: " . $tipo . "<br>" .
+                "Nome e cognome: " . $nomeUtente . " " . $cognomeUtente . "<br>" .
+                "Codice fiscale: " . $codiceFiscale . "<br>" .
+                "Data di nascita: ". $dataNascita . "<br>" .
+                "Luogo di nascita: ". $luogoNascita . "<br>" ;
+            ?>
+        </h3><br>
+        <button class='button' onclick='location.reload()'> I tuoi dati non sono corretti? Torna all'inserimento
+        </button>
+        <br><br><br>
+        Se tutti i dati sono corretti, inserisci qui sotto il codice che ti è stato inviato via e-mail per creare il tuo
+        account<br>
+        <br>
+        <form class="button" action="signin.php" method="post">
+            <input type="text" name="code" id="code" placeholder="Codice">
+            <input type="submit">
+        </form>
+    </div>
 </div>
--- userPagePassword.php
+++ userPagePassword.php
@@ -1,8 +1,6 @@
 <?php
-
-if ( ! isset($database_set)) {
+if (!isset($database_set))
     include 'database\database.php';
-}
 session_start();
 include 'checkPage.php';
 $mail = $_SESSION['mail'];
@@ -11,12 +9,12 @@ $mail = $_SESSION['mail'];
 <!DOCTYPE html>
 <html lang="it">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/header.css">
-  <link rel="stylesheet" href="./css/index.css">
-  <title>Modifica password</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Modifica password</title>
 </head>
 <body class="font">
 <style>
@@ -40,86 +38,86 @@ $mail = $_SESSION['mail'];
 
 <br>
 <div align="center">
-  <h1>Pagina di <?php
-      $nameAndSurname = retrieveNameAndSurname($mail);
-      echo $nameAndSurname['name'].' '.$nameAndSurname['surname'];
-      ?></h1>
+    <h1>Pagina di <?php
+        $nameAndSurname = retrieveNameAndSurname($mail);
+        echo $nameAndSurname['name'] . ' ' . $nameAndSurname['surname'];
+        ?></h1>
 </div>
 <br>
 
 <div align="center">
     <?php
     if (isset($_POST['old_p']))
-    {
     if (isset($_POST['new_p'])){
-    $conn      = connect_database();
+    $conn = connect_database();
     $statement = $conn->prepare("SELECT pw
                                             FROM personale
-                                            WHERE mail = '".$mail."'");
+                                            WHERE mail = '" . $mail . "'");
     $statement->execute();
-    $results      = $statement->get_result();
+    $results = $statement->get_result();
     $old_password = mysqli_fetch_row($results);
 
+    $_POST['old_p'] = hash("sha256", $_POST['old_p']);
+
     if ($_POST['old_p'] == $old_password[0]) {
-    $conn      = connect_database();
+    $conn = connect_database();
     $statement = $conn->prepare("UPDATE personale
-                                                SET pw = '".$_POST['new_p']."'
-                                                WHERE mail = '".$mail."'");
+                                                SET pw = '" . $_POST['new_p'] . "'
+                                                WHERE mail = '" . $mail . "'");
     $statement->execute();
     ?>
-  <h2>La password è stata modificata!
-    <h2><br>
-      <button class="button" onclick="window.open('userPage.php'); window.close()"> Torna ai tuoi dati</button>
-        <?php
-        } else {
-        ?>
-      <h2>La vecchia password è errata!<br>La password non è stata modificata!
+    <h2>La password è stata modificata!
         <h2><br>
-          <button class="button" onclick="window.open('userPage.php'); window.close()"> Torna ai tuoi dati
-          </button>&emsp;
-          <button class="button" onclick="window.open('userPagePassword.php'); window.close()"> Prova di
-            nuovo
-          </button>
+            <button class="button" onclick="window.open('userPage.php'); window.close()"> Torna ai tuoi dati</button>
             <?php
-            }
-            die();
-            }
-            }
-
-            if (isset($_POST['send_p'])){
-            include 'utils/emails.php';
-
-            $conn      = connect_database();
-            $statement = $conn->prepare("SELECT pw
-                                        FROM personale
-                                        WHERE mail = '".$mail."'");
-            $statement->execute();
-            $results      = $statement->get_result();
-            $old_password = mysqli_fetch_row($results);
-
-            $sendmail_message = sendEmail(
-                    $mail."@tulliobuzzi.edu.it",
-                    "Credenziali RSPP Certificati",
-                    "<h1>Password per l'accesso all'account: ".$old_password[0]."</h1>"
-            );
-
-            if ($sendmail_message != EmailStatus::OK) {
-                echo "<h2>".$sendmail_message."</h2>";
-                die();
-            }
+            } else {
             ?>
-          <h1>Al tuo indirizzo e-mail è stata mandata la tua vecchia password
-            <h1><br>
-              <button class="button" onclick="window.open('userPage.php'); window.close()"> Torna ai tuoi
-                dati
-              </button>
-                <?php
-                die();
-                }
+            <h2>La vecchia password è errata!<br>La password non è stata modificata!
+                <h2><br>
+                    <button class="button" onclick="window.open('userPage.php'); window.close()"> Torna ai tuoi dati
+                    </button>&emsp;
+                    <button class="button" onclick="window.open('userPagePassword.php'); window.close()"> Prova di
+                        nuovo
+                    </button>
+                    <?php
+                    }
+                    die();
+                    }
 
-                include 'html/user_cambia_pass.html';
+                    if (isset($_POST['send_p'])){
+                    include 'utils/emails.php';
 
-                ?>
+                    $conn = connect_database();
+                    $statement = $conn->prepare("SELECT pw
+                                        FROM personale
+                                        WHERE mail = '" . $mail . "'");
+                    $statement->execute();
+                    $results = $statement->get_result();
+                    $old_password = mysqli_fetch_row($results);
+
+                    $sendmail_message = sendEmail(
+                        $mail . "@tulliobuzzi.edu.it",
+                        "Credenziali RSPP Certificati",
+                        "<h1>Password per l'accesso all'account: " . $old_password[0] . "</h1>"
+                    );
+
+                    if ($sendmail_message != EmailStatus::OK) {
+                        echo "<h2>" . $sendmail_message . "</h2>";
+                        die();
+                    }
+                    ?>
+                    <h1>Al tuo indirizzo e-mail è stata mandata la tua vecchia password
+                        <h1><br>
+                            <button class="button" onclick="window.open('userPage.php'); window.close()"> Torna ai tuoi
+                                dati
+                            </button>
+                            <?php
+                            die();
+                            }
+
+                            include 'html/user_cambia_pass.html';
+
+                            ?>
 </div>
 </body>
 </html>
